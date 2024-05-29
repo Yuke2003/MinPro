f8325b3e9a53a698a965f313226064ab30b75c2a
soCalled devloper <111551752+Yuke2003@users.noreply.github.com>
2024-05-29 20:51:17 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://minpro-backend.onrender.com",
+        target: "https://minpro-1.onrender.com",
       },
     },
   },
