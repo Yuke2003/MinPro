844d1ab6808684f5b8ed94e33f62bc65961f374d
soCalled devloper <111551752+Yuke2003@users.noreply.github.com>
2024-05-30 08:16:42 +0530

    Update ImageUpload.jsx

--- frontend/src/Pages/ImageUpload.jsx
+++ frontend/src/Pages/ImageUpload.jsx
@@ -22,7 +22,7 @@ const ImageUpload = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:8000/api/v1/upload",
+        "https://minpro-1.onrender.com/api/v1/upload",
         formData,
         {
           headers: {
