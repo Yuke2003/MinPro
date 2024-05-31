d8ebad6c727c8cb56f735d2d32463fa7a5c8eeca
soCalled devloper <111551752+Yuke2003@users.noreply.github.com>
2024-05-31 10:35:08 +0530

    Update rentRoutes.js

--- Backend/routes/rentRoutes.js
+++ Backend/routes/rentRoutes.js
@@ -6,16 +6,16 @@ const userController = require("./../controllers/userController");
 router.route("/top-5-cheap").get(rentController.TopRents,rentController.findAllRents)
 
 router.route("/").get(userController.protect, rentController.findAllRents).post(
-  // userController.protect,
-  // userController.restrictTo("buyer"),
+  userController.protect,
+  userController.restrictTo("buyer"),
   rentController.createRent
 );
 
 router
   .route("/:id")
   .get(
-    // userController.protect,
-    // userController.restrictTo("seller"),
+    userController.protect,
+    userController.restrictTo("seller"),
     rentController.getOneRent
   )
   .patch(
