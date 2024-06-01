cb4dbb1a47923c452e7e1f55f9e1c5c406be5edc
soCalled devloper <111551752+Yuke2003@users.noreply.github.com>
2024-06-01 06:42:51 +0530

    Update emailController.js

--- Backend/controllers/emailController.js
+++ Backend/controllers/emailController.js
@@ -1,33 +1,29 @@
-const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const sgMail = require("@sendgrid/mail");
 
-dotenv.config({ path: "./../config.env" });
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-let transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
+exports.sendEmail = async (req, res, next) => {
+  try {
+    const { fromEmail, toEmail, sendSubject, sendText, sendHtml } = req.body;
+   
+    const msg = {
+      to: toEmail,
+      from: fromEmail, 
+      subject: sendSubject,
+      text: sendText,
+      html: sendHtml,
+    };
+    await sgMail.send(msg);
+    console.log("Email sent successfully!");
+  } catch (error) {
+    console.error("Error sending email:", error);
 
-exports.sendEmail = async (req, res) => {
-  const { toEmail, fromEmail, subject, message } = req.body;
-
-  var mailOptions = {
-    from: fromEmail,
-    to: toEmail,
-    subject: subject,
-    text: message,
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent successfully!");
+    if (error.response) {
+      console.error("SendGrid response:", error.response.body);
     }
-  });
+  }
+  next();
 };
+
